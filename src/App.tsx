492b8717b373952cd41344981ca85e4a6f3392e0
Ivan <ichimbobautista@gmail.com>
2025-07-28 09:49:10 -0500

    algo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { About } from "./components/About";
 import { Cta } from "./components/Cta";
-import { FAQ } from "./components/FAQ";
+// import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-import { Newsletter } from "./components/Newsletter";
+// import { Newsletter } from "./components/Newsletter";
 // import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { Services } from "./components/Services";
 // import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
+// import { Team } from "./components/Team";
+// import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
@@ -26,11 +26,11 @@ function App() {
       <Features />
       <Services />
       <Cta />
-      <Testimonials />
-      <Team />
+      {/* <Testimonials /> */}
+      {/* <Team /> */}
       {/* <Pricing /> */}
-      <Newsletter />
-      <FAQ />
+      {/* <Newsletter /> */}
+      {/* <FAQ /> */}
       <Footer />
       <ScrollToTop />
     </>
